cb9e0c4b6ba5782165f0580d9aaa7d581e0d3055
Matheus dos Santos Paixão <matheusworkcontact@gmail.com>
2026-01-31 12:48:51 -0300

    refactor commands

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -1,12 +1,17 @@
 import type { Command } from "commander";
-import build from "./commands/build";
-import dev from "./commands/dev";
+import build from "./commands/app/build";
+import dev from "./commands/app/dev";
+import start from "./commands/app/start";
 import init from "./commands/init";
-import start from "./commands/start";
+import create from "./commands/routes/create";
 
 export function registerCommands(program: Command) {
+  // App
   program.addCommand(init);
   program.addCommand(dev);
   program.addCommand(build);
   program.addCommand(start);
+
+  // Routes
+  program.addCommand(create);
 }
